cfcd324442c4b446b4d64c06507d9ae50d49521a
Jhonnata Nascimento da Paixão <114832000+jhownpaixao@users.noreply.github.com>
2023-08-25 14:35:51 -0300

    fix

--- src/Bases/ModelStaticFunctions.php
+++ src/Bases/ModelStaticFunctions.php
@@ -107,7 +107,7 @@ trait ModelStaticFunctions
             $m = new $m;
 
             $operator = OperatorResolver::get(static::class);
-
+            $params['limit'] = 1;
             $query = $operator->mountQuery($m->getTablename(), $params);
             /**
              * @var Connection
